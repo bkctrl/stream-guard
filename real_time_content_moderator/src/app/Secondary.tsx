465503499989aa20303fb58e5dee4302a2429f4b
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 17:22:05 -0400

    styling

--- real_time_content_moderator/src/app/Secondary.tsx
+++ real_time_content_moderator/src/app/Secondary.tsx
@@ -1,3 +1,4 @@
+import Webcam from "react-webcam";
 
 const Secondary: React.FC = () => {
 
@@ -6,33 +7,37 @@ const Secondary: React.FC = () => {
       className="w-full h-svh bg-slate-100 flex justify-center"
     >
       <div
-        className="w-3/4 h-full bg-slate-300 flex flex-col pb-12"
+        className="w-3/4 shadow-2xl h95 bg-slate-300 flex flex-col pb-12 rounded-xl"
       >
         <div 
-          className="w-full h-24 bg-slate-500"
+          className="w-full h-24 bg-slate-300 rounded-xl"
         >
           <div>
-            <h2>Try the demo!</h2>
+            <h2
+              className="text-6xl mt-6 ml-12"
+            >
+              Try the demo!
+            </h2>
           </div>
         </div>
 
         <div className="w-full h-full flex">
           <div className="w-2/3 h-full">
-            <div className="mt-3.5 ml-4 pt-0.5 h-full w-full gbg1">
-              <div className="mt-4 ml-4 h-full w-full bg-cyan-50">
-                <div className="h-3/4 border border-slate-400">
-                  <input type="file" accept="image/*" capture="camera" />
+            <div className="mt-4 ml-4 pt-0.5 h-full w-full gbg1">
+              <div className="mt-4 ml-4 h-full w-full">
+                <div className="h-3/4 border border-slate-400 bg-cyan-50">
+                  {/* <Webcam /> */}
                 </div>
-                <div className="flex h-1/4 border border-slate-400">
+                <div className="flex h-1/4 border border-slate-400 bg-white">
                   <div 
-                    className="w-full h-1/4 border-b-2 flex"
+                    className="w-full h-1/3 border-b-2 flex"
                   >
                     <div className="w-1/3 h-full">
                     
                     </div>
                     <div className="w-1/3 h-full flex justify-center items-center">
                       <button
-                        className="size-12 rounded-3xl bg-slate-200 "
+                        className="size-12 rounded-3xl bg-white border border-black"
                       >
                         mic
                       </button>
@@ -48,9 +53,10 @@ const Secondary: React.FC = () => {
             </div>
           </div>
 
-          <div className="w-1/3 h-full m-4 ml-12 bg-black">
-            <p>transcript</p>
+          <div className="w-1/3 p-4 h-auto m-4 ml-12 rounded-lg bg-white border-2 border-slate-500">
+            <p>transcript goes here</p>
           </div>
+
 
 
         </div>
