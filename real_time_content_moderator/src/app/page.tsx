54172dcfe93c44bd8eb8ae20515038874c9983b5
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 14:00:04 -0400

    file modifications

--- real_time_content_moderator/src/app/page.tsx
+++ real_time_content_moderator/src/app/page.tsx
@@ -1,12 +1,14 @@
 import HeaderBar from "./HeaderBar";
 import Banner from "./Banner";
 import Footer from "./Footer";
+import Secondary from "./Secondary";
 
 export default function Home() {
   return (
     <>
       <HeaderBar />
       <Banner />
+      <Secondary />
       <Footer />
     </>
   );
