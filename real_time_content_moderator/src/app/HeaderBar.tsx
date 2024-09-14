aedc515e07d18b7183d10bfde8bcf4341265de0a
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 03:07:41 -0400

    pre merge

--- real_time_content_moderator/src/app/HeaderBar.tsx
+++ real_time_content_moderator/src/app/HeaderBar.tsx
@@ -1,20 +1,21 @@
 export default function HeaderBar() {
   return (
-    <div className="h-16 w-full flex border-2 border-slate-800">
-      <div className="flex justify-center items-center ml-8">
+    <div className="h-16 w-full flex justify-between px-8 border-2 border-slate-800">
+      <div className="flex justify-center items-center">
         <h2>
           Real Time Content Moderator
         </h2>
 
-        <div>
-          <button>
-            Sign In
-            <i></i>
-          </button>
-        </div>
       </div>
 
-
+      <div className="flex justify-center items-center">
+        <button
+          className="bg-amber-400 hover:bg-amber-300 focus:bg-amber-500 px-4 py-1.5 border-2 border-slate-400 rounded-3xl"
+        >
+          Sign In
+          <i></i>
+        </button>
+      </div>
 
 
 
