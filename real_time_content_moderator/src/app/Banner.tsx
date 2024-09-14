191ca8a5a524f47ffa9cf08a3efa66b82fea2131
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 02:49:17 -0400

    added Banner component

---
+++ real_time_content_moderator/src/app/Banner.tsx
@@ -0,0 +1,16 @@
+export default function Banner() {
+
+
+  return (
+    <div className="diagonal-split flex w-full h-96 border-purple-600">
+      <div className="h-full flex justify-center items-center w-2/3">
+        <span className="mx-10 font-medium text-7xl">
+          Remove explicit content from your audio <br /> <strong>in real time.</strong>
+        </span>
+      </div>
+      <div className="h-full flex justify-center w-1/3">
+
+      </div>
+    </div>
+  );
+}
