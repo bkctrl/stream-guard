8d4d509f64d16367cc269942753f5396f4912ba9
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 13:59:13 -0400

    new component

---
+++ real_time_content_moderator/src/app/Secondary.tsx
@@ -0,0 +1,14 @@
+
+
+const Secondary: React.FC = () => {
+
+  return (
+    <div 
+      className="w-full h-svh bg-slate-100"
+    >
+      <p>secondary</p>
+    </div>
+  );
+}
+
+export default Secondary;
