0342cdc550b6a3fe74f156d225a065ec4e1565fb
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 02:30:42 -0400

    basic header layout

---
+++ real_time_content_moderator/src/app/HeaderBar.tsx
@@ -0,0 +1,16 @@
+export default function HeaderBar() {
+  return (
+    <div className="h-16 w-full flex border-2 border-slate-800">
+      <div className="flex justify-center items-center ml-8">
+        <h1>
+          Real Time Content Moderator
+        </h1>        
+      </div>
+
+
+
+
+
+    </div>
+  );
+}
