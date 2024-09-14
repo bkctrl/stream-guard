7afc5e1007e6d6a475af71562f0561637b5fb57e
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 02:50:05 -0400

    modifications to component files

--- real_time_content_moderator/src/app/HeaderBar.tsx
+++ real_time_content_moderator/src/app/HeaderBar.tsx
@@ -2,9 +2,16 @@ export default function HeaderBar() {
   return (
     <div className="h-16 w-full flex border-2 border-slate-800">
       <div className="flex justify-center items-center ml-8">
-        <h1>
+        <h2>
           Real Time Content Moderator
-        </h1>        
+        </h2>
+
+        <div>
+          <button>
+            Sign In
+            <i></i>
+          </button>
+        </div>
       </div>
 
 
