ec2a5408e45e2b23d562e61f13bf0c831974bc54
ashsic <99445200+ashsic@users.noreply.github.com>
2024-09-14 20:19:45 -0400

    timer functionality

--- real_time_content_moderator/src/app/Secondary.tsx
+++ real_time_content_moderator/src/app/Secondary.tsx
@@ -1,9 +1,64 @@
 "use client"
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
 
 const Secondary: React.FC = () => {
   const webcamRef = useRef(null);
+  const timerRef: any = useRef(null);
+  const startRef = useRef(null);
+
+  const [seconds, setSeconds] = useState(0);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+  const toggleTimer = () => {
+    setIsTimerRunning(
+      isTimerRunning => !isTimerRunning
+    )
+  }
+
+  useEffect(() => {
+    let intervalId: any;
+
+    if (isTimerRunning) {
+      intervalId = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    }
+    
+
+    return () => clearInterval(intervalId); // Clean up interval on component unmount
+}, [isTimerRunning]);
+
+useEffect(() => {
+    if (timerRef.current) {
+      let hours = 0;
+      let minutes = 0;
+      let curr = seconds;
+      if (curr > 3600) {
+        hours = curr % 3600;
+        curr -= hours * 3600;
+      }
+
+      if (curr > 60) {
+        minutes = curr % 60;
+        curr -= minutes * 60; 
+      }
+    
+      let secZero = "";
+      if (curr < 10) {
+        secZero = "0";
+      }
+
+      let minZero = "";
+      if (minutes < 10) {
+        minZero = "0";
+      }
+
+      timerRef.current.textContent = `${hours}:${minZero}${minutes}:${secZero}${curr}`;
+    }
+}, [seconds]); // Update the text content whenever the count changes
+
+
   return (
     <div 
       className="w-full h-svh bg-slate-100 flex justify-center"
@@ -27,11 +82,14 @@ const Secondary: React.FC = () => {
           <div className="w-2/3 h-full">
             <div className="mt-4 ml-4 pt-0.5 h-full w-full gbg1">
               <div className="mt-4 ml-4 h-full w-full">
-                <div className="h-3/4 border border-slate-400 bg-cyan-50" 
-                  style={{width: "35.5rem", height: "20rem"}}>
-                  <Webcam />
+                <div className="h-3/4 border  bg-cyan-50" 
+                  // style={{width: "35.5rem", height: "20rem"}}
+                >
+                  <Webcam 
+                    className='w-full h-fit'
+                  />
                 </div>
-                <div className="flex flex-col h-1/4 border border-slate-400 bg-white">
+                <div className="flex flex-col h-1/4 border  bg-white">
                   <div 
                     className="w-full h-1/3 border-b-2 flex"
                   >
@@ -41,6 +99,8 @@ const Secondary: React.FC = () => {
                     <div className="w-1/3 h-full flex justify-center items-center">
                       <button
                         className="size-12 rounded-3xl bg-white border border-black"
+                        id="start"
+                        onClick={toggleTimer}
                       >
                         mic
                       </button>
@@ -57,6 +117,7 @@ const Secondary: React.FC = () => {
                       <span 
                           id="time"
                           className="text-6xl h-1/2" 
+                          ref={timerRef}
                         >
                           0:00:00
                         </span>
